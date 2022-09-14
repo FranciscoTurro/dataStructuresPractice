16092ba9072668591488c6273cf0cf25ecc16351
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-09-14 14:14:11 -0300

    deletenode doesnt work

--- binarySearchTrees/binarySearchTrees.js
+++ binarySearchTrees/binarySearchTrees.js
@@ -2,12 +2,10 @@ const Node = (data) => {
   value = data;
   right = null;
   left = null;
-  return { data, right, left };
+  return { value, right, left };
 };
 
 const Tree = (array) => {
-  root = null;
-
   const buildTree = (array, start, end) => {
     if (start > end) {
       return null;
@@ -20,29 +18,74 @@ const Tree = (array) => {
     return node;
   };
 
-  const mergeSort = (array) => {
-    if (array.length === 1) return array;
-    const newArray = [];
-    const leftPart = mergeSort(array.slice(0, array.length / 2));
-    const rightPart = mergeSort(array.slice(array.length / 2));
-    while (leftPart.length && rightPart.length) {
-      if (leftPart[0] < rightPart[0]) {
-        newArray.push(leftPart.shift());
-      } else {
-        newArray.push(rightPart.shift());
-      }
+  rootNode = buildTree(array, 0, array.length - 1);
+
+  const insertNode = (value, root = rootNode) => {
+    if (root === null) {
+      root = Node(value);
+      return root;
     }
-    return [...newArray, ...leftPart, ...rightPart];
+    if (value < root.value) {
+      root.left = insertNode(value, root.left);
+    } else if (value > root.value) {
+      root.right = insertNode(value, root.right);
+    }
+    return root;
   };
 
-  const elimDuplicates = (array) => {
-    return [...new Set(mergeSort(array))];
+  const deleteNode = (value, root = rootNode) => {
+    if (root == null) {
+      return root;
+    }
+    if (value < root.value) {
+      root.left = deleteNode(value, root.left);
+    } else if (value > root.value) {
+      root.right = deleteNode(value, root.right);
+    } else {
+      if (root.left == null) {
+        return root.right;
+      } else if (root.right == null) {
+        return root.left;
+      }
+      root.value = minValue(root.right);
+      root.right = deleteNode(root.value, root.right);
+    }
   };
 
-  return { buildTree };
+  const minValue = (root) => {
+    let minv = root.value;
+    while (root != null) {
+      minv = root.value;
+      root = root.left;
+    }
+    return minv;
+  };
+
+  return { insertNode, rootNode, deleteNode };
+};
+
+const mergeSort = (array) => {
+  if (array.length === 1) return array;
+  const newArray = [];
+  const leftPart = mergeSort(array.slice(0, array.length / 2));
+  const rightPart = mergeSort(array.slice(array.length / 2));
+  while (leftPart.length && rightPart.length) {
+    if (leftPart[0] < rightPart[0]) {
+      newArray.push(leftPart.shift());
+    } else {
+      newArray.push(rightPart.shift());
+    }
+  }
+  return [...newArray, ...leftPart, ...rightPart];
+};
+
+const elimDuplicates = (array) => {
+  return [...new Set(mergeSort(array))];
 };
 
 const asd = [1, 2, 3, 4, 5, 6, 7];
 const ass = Tree(asd);
-tree = ass.buildTree(asd, 0, asd.length - 1);
-console.log(tree);
+console.log(ass.rootNode);
+ass.deleteNode(7);
+console.log('after deletion');
+console.log(ass.rootNode);
